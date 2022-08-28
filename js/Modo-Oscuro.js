508bd7e14a59ac3ffc380f8dabadcab498c55592
nicolasgodoy <nicolasgodoy540@gmail.com>
2022-08-28 15:32:44 -0300

    Actualizado

---
+++ js/Modo-Oscuro.js
@@ -0,0 +1,22 @@
+
+
+ /* =========  Modo oscuro y Modo Claro ======== */
+
+const boton = document.querySelector('#Correr-boton');
+
+
+boton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    boton.classList.toggle('active');
+    
+
+});
+
+/* ======= Modo oscuro y Modo Claro  ======= */
+
+
+
+
+
+
+
